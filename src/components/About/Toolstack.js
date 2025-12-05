88e144b5d8d57e31743c36d6352f4589ecbdb7fa
Nishant <118586587+nishant1810@users.noreply.github.com>
2025-12-05 22:48:49 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -5,23 +5,32 @@ import {
   SiPostman,
   SiVercel,
   SiGithub,
+  SiDocker,
+  SiLinux,
+  SiHeroku,
+  SiNetlify,
+  SiRender,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiGithub />
-      </Col>
+      {/* IDE & Development */}
+      <Col xs={4} md={2} className="tech-icons"><SiVisualstudiocode /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiPostman /></Col>
+
+      {/* Version Control */}
+      <Col xs={4} md={2} className="tech-icons"><SiGithub /></Col>
+
+      {/* Deployment Platforms */}
+      <Col xs={4} md={2} className="tech-icons"><SiVercel /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiRender /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiNetlify /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiHeroku /></Col>
+
+      {/* DevOps & Cloud */}
+      <Col xs={4} md={2} className="tech-icons"><SiDocker /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiLinux /></Col>
     </Row>
   );
 }
